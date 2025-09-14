48f816e0fd513de8a96d3b5d8927bc048bfbd810
VUANHKHOA <khoava06@gmail.com>
2025-09-14 20:26:43 +0700

    Undo_DSTTmangcontro

--- mylib.h
+++ mylib.h
@@ -1,30 +1,104 @@
-#ifndef MYLIB_H
-#define MYLIB_H
-
-#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dos.h>
+#include <string.h>
 #include <windows.h>
-#include <iomanip>
-using namespace std;
 
-HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-
- void setColor(int color) {
-    SetConsoleTextAttribute(h, color);
+#define Enter 13
+//const int WHITE=15;
+#define PASSWORD "abcdef"
+//const int WHITE=15;
+ 
+char* Pwd () { 
+     char S[40]; int i=0;
+     while ((S[i]= getch()) != Enter ) 
+     { printf ("%c", '*') ; i++ ; 
+     }
+     S[i]='\0';
+     return S;
+}
+int CheckPwd () {
+    int dem =0; 
+    for ( dem =1 ; dem <=3 ; dem++)
+    { printf( "Password :"); 
+      if (strcmp(Pwd(),PASSWORD) ==0)   return 1;
+      else printf ( "\nPassword sai. Hay nhap lai\n")  ; 
+    }
+    return 0;  
 }
 
- void gotoxy(int x, int y) {
-    COORD coord;
-    coord.X = x;
-    coord.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+void gotoxy(short x,short y)
+{
+        HANDLE hConsoleOutput;
+        COORD Cursor_an_Pos = { x,y};
+        hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleCursorPosition(hConsoleOutput , Cursor_an_Pos);
+}  
+
+int wherex( void )
+{
+    HANDLE hConsoleOutput;  
+    
+    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+    GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
+    return screen_buffer_info.dwCursorPosition.X;
 }
 
- void drawBox(int x, int y, string content, int color = 7) {
-    setColor(color);
-    gotoxy(x, y);     cout << "+---+";
-    gotoxy(x, y + 1); cout << "| " << setw(2) << content << " |";
-    gotoxy(x, y + 2); cout << "+---+";
-    setColor(7);
+int wherey( void )
+{
+    HANDLE hConsoleOutput;
+    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+    GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
+    return screen_buffer_info.dwCursorPosition.Y;
+}
+void clreol( ) {
+COORD coord;
+DWORD written;
+CONSOLE_SCREEN_BUFFER_INFO info;
+GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+coord.X = info.dwCursorPosition.X;
+coord.Y = info.dwCursorPosition.Y;
+FillConsoleOutputCharacter (GetStdHandle(STD_OUTPUT_HANDLE), ' ',
+  info.dwSize.X - info.dwCursorPosition.X * info.dwCursorPosition.Y, coord, &written);
+gotoxy (info.dwCursorPosition.X , info.dwCursorPosition.Y );
 }
 
-#endif
+void SetColor(WORD color)
+{
+    HANDLE hConsoleOutput;
+    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+    GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
+
+    WORD wAttributes = screen_buffer_info.wAttributes;
+    color &= 0x000f;
+    wAttributes &= 0xfff0;
+    wAttributes |= color;
+
+    SetConsoleTextAttribute(hConsoleOutput, wAttributes);
+}
+void SetBGColor(WORD color)
+{
+    HANDLE hConsoleOutput;
+    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+    GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
+
+    WORD wAttributes = screen_buffer_info.wAttributes;
+    color &= 0x000f;
+    color <<= 4;
+    wAttributes &= 0xff0f;
+    wAttributes |= color;
+
+    SetConsoleTextAttribute(hConsoleOutput, wAttributes);
+}
+void clrscr() {
+	system("cls");
+}
+
+
